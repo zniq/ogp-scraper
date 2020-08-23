23424b0e85ca93bf68c520c41736a5020c358593
Osamu Sato <zniq1129@gmail.com>
2020-08-24 00:55:31 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,21 @@
+## OGP-Scraper
+
+This is a commandline tool for scraping OGP tags, Written in Node.js.
+
+The target tag:
+- og:xxx
+- fb:xxx
+- twitter:xxx
+  
+These existing tags will be returned in JSON.
+
+## install
+```
+yarn install
+```
+
+## use
+```
+node app.js {url}
+```
+
---
+++ app.js
@@ -0,0 +1,53 @@
+const cli = require('cheerio-httpcli');
+
+async function main(argv) {
+    const args = parseArgs(argv.slice(2));
+    if (!args) {
+        console.log("args[0]: access uri not found.");
+        return;
+    }
+
+    const res = await scrape(args);
+    console.log(JSON.stringify(res));
+};
+main(process.argv);
+
+function parseArgs(args) {
+    if (!args.length) {
+        return null;
+    }
+
+    return {
+        uri: args[0]
+    };
+}
+
+async function scrape(args) {
+    cli.setBrowser('chrome');
+    const ats = await cli.fetch(args.uri)
+        .then(filter)
+        .catch(console.error);
+
+    return ats;
+}
+
+function filter({error: err, $, response: res, body}) {
+    if (err) {
+        console.error("fetch error: ", err);
+        return null;
+    }
+    const og = {key: 'og', selector: `meta[property^="og:"]`};
+    const fb = {key: 'facebook', selector: `meta[property^="fb:"]`};
+    const tw = {key: 'twitter', selector: `meta[property^="twitter:"]`};
+
+    const mapper = (p) => {
+        const elms = Array.from($(p.selector).map((i, e) => e.attribs));
+        const props = elms.reduce((acc, e) => {
+            const a = [e.property.split(':').slice(1).join(':'), e.content];
+            return [...acc, a];
+        }, []);
+
+        return [p.key, Object.fromEntries(props)];
+    };
+    return Object.fromEntries([og, fb, tw].map(mapper));
+}
